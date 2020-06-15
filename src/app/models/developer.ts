a02d351fb6b5d27a42b1a29a74841efdc4b2db0a
Ryan Acevedo <pair+ryan@spacecamp.ninja>
2020-06-15 11:35:55 -0600

    created baseline for developer/project models

--- src/app/models/developer.ts
+++ src/app/models/developer.ts
@@ -1,2 +1,28 @@
 export class Developer {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _title: string
+  ) { }
+
+
+  get title(): string {
+    return this._title;
+  }
+  set title(value: string) {
+    this._title = value;
+  }
+  get name(): string {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+  get id(): number {
+    return this._id;
+  }
+  set id(value: number) {
+    this._id = value;
+  }
+
 }
--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,2 +1,34 @@
 export class Project {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _ationYear: number,
+    private _owner?: any[]
+  ) { }
+
+
+  get owner(): any[] {
+    return this._owner;
+  }
+  set owner(value: any[]) {
+    this._owner = value;
+  }
+  get ationYear(): number {
+    return this._ationYear;
+  }
+  set ationYear(value: number) {
+    this._ationYear = value;
+  }
+  get name(): string {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+  get id(): number {
+    return this._id;
+  }
+  set id(value: number) {
+    this._id = value;
+  }
 }
